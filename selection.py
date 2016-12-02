1e7d5908b86a3228c61401af51919d775528e9ed
Habib Fikri <habibgarut@gmail.com>
2016-12-02 11:13:19 +0700

    Menyamakan line dengan insertion

--- selection.py
+++ selection.py
@@ -5,8 +5,7 @@ def selectionSort(arr):
 		minidx = x
 		y = x + 1
 		while y < lenarr:
-			if arr[y] < arr[minidx]:
-				minidx = y
+			if arr[y] < arr[minidx]: minidx = y
 			y += 1
 		arr[x], arr[minidx] = arr[minidx], arr[x]
 		x += 1
